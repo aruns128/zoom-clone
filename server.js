00085ddcd04516974463d2a86ffaa24299386d32
Arun <arunana2017@gmail.com>
2020-08-14 17:39:46 +0530

    env port updated

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
-const { ExpressPeerServer } = require('peer')
+const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
-})
+});
+const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer)
+app.use('/peerjs', peerServer);
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -28,7 +29,7 @@ io.on('connection', socket => {
     socket.on('message', (message) => {
       //send message to the same room
       io.to(roomId).emit('createMessage', message)
-    });
+  }); 
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
@@ -36,4 +37,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+server.listen(process.env.PORT||3030)
